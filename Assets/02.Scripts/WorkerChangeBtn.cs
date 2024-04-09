1daad5856ee3733b8e6e1f58a46de71d2f7c13d8
decker <dkzmfhem@daum.net>
2024-04-10 03:59:04 +0900

    클릭 시 근무자 지정버튼 초기화

--- Assets/02.Scripts/WorkerChangeBtn.cs
+++ Assets/02.Scripts/WorkerChangeBtn.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using sgSchedule;
 public class WorkerChangeBtn : MonoBehaviour {
     public WorkerChange workerChange;
     public void ClickWorkerBtn () {
         ScheduleHolidayItem item = gameObject.GetComponent<ScheduleHolidayItem> ();
         string workerName = gameObject.GetComponentInChildren<TMP_Text>().text;
+        if(workerName =="미정")
+        {
+            Main.AlertAction("근무자가 지정되지 않은 날짜입니다.");
+            return;
+        }
+        workerChange.gameObject.SetActive(false);
+        workerChange.gameObject.SetActive(true);
+
         workerChange.ClickWorker = workerName;
         workerChange.thatDate = item.myDate;
         workerChange.gameObject.SetActive (true);
